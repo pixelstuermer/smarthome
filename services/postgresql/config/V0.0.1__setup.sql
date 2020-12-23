136530e9c2120de32417ded17deda94eeb04aa2e
Philipp Montesano <philipp.montesano@icloud.com>
2020-12-23 13:03:52 +0100

    Granting rights for PostgreSQL backend user

--- services/postgresql/config/V0.0.1__setup.sql
+++ services/postgresql/config/V0.0.1__setup.sql
@@ -4,3 +4,7 @@ CREATE SCHEMA smarthome;
 
 CREATE ROLE backend LOGIN PASSWORD 'md58f03c909bc9ab63255941fb86a434e27';
 ALTER ROLE backend SET search_path TO '$user', smarthome, public;
+
+GRANT CONNECT ON DATABASE smarthome TO backend;
+GRANT USAGE ON SCHEMA smarthome TO backend;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA smarthome TO backend;
